28378ec1cca4256725ae6070dfac0ed35642d190
Morris Anthony <morris.anthony2992@gmail.com>
2021-05-16 01:16:50 -0400

    removed env.production and deployment files

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,10 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        spaceId: `${process.env.GATSBY_CONTENTFUL_SPACE_ID}`,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        host: `cdn.contentful.com`,
+        accessToken: `${process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN}`,
       },
     },
     `gatsby-transformer-sharp`,
